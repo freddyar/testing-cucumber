25eb26f1523263e5159288da136999f9b8cf9125
Freddy Aguilar Rojas <freddy.aguilar@et.bo>
2022-07-21 20:14:07 -0400

    First, second and trird tests

--- src/test/java/runner/MyStepdefs.java
+++ src/test/java/runner/MyStepdefs.java
@@ -11,30 +11,30 @@ import io.cucumber.java.en.When;
 public class MyStepdefs {
 
     /*  regular expression / cucumber expression
-     *  {}      --- convertir varias palabras en argumentos
+     *  {} --- convertir varias palabras en argumentos
      *  {string}---- convertir una palabra en comillas dobles "valor" como argumento
-     *  {int}   --- convertir un valor int como argumento
-     *  {float} --- convertir valores flotantes como argumento
-     *  {word}  --- convertir una palabra en argumento
+     *  {int}--- convertir un valor int como argumento
+     *  {float}--- convertir valores flotantes como argumento
+     *  {word} --- convertir una palabra en argumento
      * */
 
     @Given("tengo acceso a {}")
-    public void tengoAccesoATodoLy(String appUrl) {
+    public void tengoAccesoATodoLy(String app) {
     }
 
-    @When("realizo click en el botón login")
-    public void realizoClickEnElBotónLogin() {
+    @When("realizo click en el boton login")
+    public void realizoClickEnElBotonLogin() {
     }
 
-    @And("ingreso mi correo electrónico: {string}")
-    public void ingresoMiCorreoElectrónicoMaestriaMaestriaCom(String email) {
+    @And("ingreso mi correo electronico: {string}")
+    public void ingresoMiCorreoElectronico(String arg0) {
     }
 
     @And("ingreso mi password: {int}")
-    public void ingresoMiPassword(int arg0) {
+    public void ingresoMiPasswordPassword(int pass) {
     }
 
-    @Then("debería ingresar a la aplicación")
-    public void deberíaIngresarALaAplicación() {
+    @Then("deberia ingresar a la aplicacion")
+    public void deberiaIngresarALaAplicacion() {
     }
 }
--- src/test/resources/myFirstCucumber.feature
+++ src/test/resources/myFirstCucumber.feature
@@ -1,8 +1,13 @@
 Feature: Login
-  Scenario: Como usuario, quiero ingresar a la aplicación usando email y password para revisar mis archivos
+
+  Scenario: Como usuario quiero ingresar a la aplicacion usando
+  email y pwd para revisar mis archivos
+
     Given tengo acceso a todo.ly
-    When realizo click en el botón login
-    And ingreso mi correo electrónico: "maestria@maestria.com"
+    When realizo click en el boton login
+    And ingreso mi correo electronico: "eynar@eynar.com"
     And ingreso mi password: 12345
-    And realizo click en el botón login
-    Then debería ingresar a la aplicación
+    And realizo click en el boton login
+    Then deberia ingresar a la aplicacion
+
+
